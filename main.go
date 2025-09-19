37fd577ec8c9d0404222d1a9ea938501b906be3b
Biljana <biljanakatanic7@gmail.com>
2025-09-19 15:30:51 +0200

    Proxy requests to stackeholders service

--- main.go
+++ main.go
@@ -82,7 +82,7 @@ func main() {
 	// -------- CORS handler --------
 	muxWithCORS := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:4200")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PATCH")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, PATCH")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
 		if r.Method == "OPTIONS" {
